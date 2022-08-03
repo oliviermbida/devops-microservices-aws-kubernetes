a68d0677079b001b6513e6dec273ee38537dbe73
Olivier Mbida <olivier.mbida@ai-uavsystems.com>
2022-08-03 09:29:54 +0000

    Added ecr upload to circleci

--- upload_docker.sh
+++ upload_docker.sh
@@ -4,14 +4,12 @@
 # Assumes that an image is built via `run_docker.sh`
 # Install dependencies
 sudo apt install -y jq 
-# REPO_EXIST=$(aws ecr describe-repositories --output text --query "repositories[].repositoryName" | grep -oh "ai-cloudsolutions")
-# if [ $REPO_EXIST != "ai-cloudsolutions" ]; then \
-#     aws ecr create-repository \
-#         --repository-name ai-cloudsolutions \
-# fi
+# ECR repo
+AWS_ECR_REPO=ai-cloudsolutions
 # Get AWS details
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 AWS_REGION=$(aws configure get region)
+AWS_ECR_ACCOUNT_URL="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
 # Get dockerhub credentials from aws secretsmanager
 DOCKERHUB_USERNAME=$(aws ssm get-parameter \
     --name /aws/reference/secretsmanager/dockerhub \
@@ -27,15 +25,25 @@ DOCKERHUB_PASSWORD=$(aws ssm get-parameter \
 dockerpath=$(printf "%s/%s" "${DOCKERHUB_USERNAME}" "flask-app-prediction")
 # Step 2:  
 # Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
+echo "Docker ID and Image: ${dockerpath}"
 echo "${DOCKERHUB_PASSWORD}" | docker login -u "${DOCKERHUB_USERNAME}" --password-stdin 
 docker build --tag "$dockerpath:latest" .
 docker image ls
 # Step 3:
 # Push image to a docker repository
-docker push "$dockerpath:latest"
-#Push image to AWS ECR
-docker tag "$dockerpath:latest" $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/ai-cloudsolutions:flask-app-prediction
+docker push "${dockerpath}:latest"
+#Push image to AWS ECR private repository
+aws ecr describe-repositories --region "${AWS_REGION}" --repository-names "${AWS_ECR_REPO}" >/dev/null 2>&1 || \
+    aws ecr create-repository --region "${AWS_REGION}" --repository-name "${AWS_ECR_REPO}" 
+# ECR public
+# aws ecr-public describe-repositories --region "${AWS_REGION}" --repository-names "${ECR_REPO}" >/dev/null 2>&1 || \
+#     aws ecr-public create-repository --region "${AWS_REGION}" --repository-name "${ECR_REPO}" 
+    
+docker tag "${dockerpath}:latest" "${AWS_ECR_ACCOUNT_URL}/${AWS_ECR_REPO}:flask-app-prediction"
 docker image ls
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/ai-cloudsolutions:flask-app-prediction
+if cat ~/.docker/config.json | grep "${AWS_ECR_ACCOUNT_URL}" > /dev/null 2>&1 ; then 
+    echo "ECR credential helper is already installed" 
+else 
+    aws ecr get-login-password --region "${AWS_REGION}" | docker login --username AWS --password-stdin "${AWS_ECR_ACCOUNT_URL}" 
+fi
+docker push "${AWS_ECR_ACCOUNT_URL}/${AWS_ECR_REPO}:flask-app-prediction"
